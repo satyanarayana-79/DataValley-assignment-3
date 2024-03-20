7f6f6d1d0f2237077fd8c6e13a498a272c48b116
satyanarayana-79 <113083428+satyanarayana-79@users.noreply.github.com>
2024-03-20 20:00:56 +0530

    Create Synchronization.java

---
+++ Synchronization.java
@@ -0,0 +1,60 @@
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class IncrementThread extends Thread {
+    private Counter counter;
+
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
+
+class DecrementThread extends Thread {
+    private Counter counter;
+
+    public DecrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.decrement();
+        }
+    }
+}
+
+public class SynchronizationExample {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        IncrementThread incrementThread = new IncrementThread(counter);
+        DecrementThread decrementThread = new DecrementThread(counter);
+
+        incrementThread.start();
+        decrementThread.start();
+
+        incrementThread.join();
+        decrementThread.join();
+
+        System.out.println("Final count value: " + counter.getCount());
+    }
+}
